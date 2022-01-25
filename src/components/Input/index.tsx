de01d239c0398d8a49da373fe7cfe06583e3b444
José Victor <josevictornascimento2016@gmail.com>
2022-01-25 18:57:06 -0300

    :tada: Complementary challenge finalized

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,13 +4,21 @@ import {
   useState,
   useCallback,
 } from 'react';
+import { IconBaseProps } from 'react-icons/lib';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props {
+  name: string
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+type InputProps = JSX.IntrinsicElements['input'] & Props
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
